7c2a6ef2e34fc0ef4352f3c6e112e7a94f9f6d0d
Станислав В. Греков <sgrekov@horizon.local>
2023-03-16 05:21:48 +0000

    Another commit2

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
